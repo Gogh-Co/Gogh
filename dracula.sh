9aa2f88fe5d29864229a36f09ee8e0453aa6bd70
Mayccoll <mayccoll@gmail.com>
2015-05-19 23:00:37 -0500

    Dracula - new theme

---
+++ dracula.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+
+# ====================CONFIG THIS =============================== #
+
+BACKGROUD_COLOR="#282a36"    # Background Color
+FOREGROUND_COLOR="#f8f8f2"   # Text
+
+COLOR_01="#44475a"           # HOST
+COLOR_02="#ff5555"           # SYNTAX_STRING
+COLOR_03="#50fa7b"           # COMMAND
+COLOR_04="#ffb86c"           # COMMAND_COLOR2
+COLOR_05="#8be9fd"           # PATH
+COLOR_06="#bd93f9"           # SYNTAX_VAR
+COLOR_07="#ff79c6"           # PROMP
+COLOR_08="#94A3A5"           #
+
+COLOR_09="#000000"           #
+COLOR_10="#ff5555"           # COMMAND_ERROR
+COLOR_11="#50fa7b"           # EXEC
+COLOR_12="#ffb86c"           #
+COLOR_13="#8be9fd"           # FOLDER
+COLOR_14="#bd93f9"           #
+COLOR_15="#ff79c6"           #
+COLOR_16="#ffffff"           #
+
+# =============================================================== #
+
+
+# |
+# | Convert RGB to gnome colors
+# | ===========================================
+function gnome_color () {
+    AA=${1:1:2}
+    BB=${1:3:2}
+    CC=${1:5:2}
+
+    echo "#${AA}${AA}${BB}${BB}${CC}${CC}"
+}
+
+
+
+# |
+# | Set gnome Variables
+# | ===========================================
+
+BACKGROUD_COLOR=$(gnome_color $BACKGROUD_COLOR)
+FOREGROUND_COLOR=$(gnome_color $FOREGROUND_COLOR)
+
+COLOR_01=$(gnome_color $COLOR_01)
+COLOR_02=$(gnome_color $COLOR_02)
+COLOR_03=$(gnome_color $COLOR_03)
+COLOR_04=$(gnome_color $COLOR_04)
+COLOR_05=$(gnome_color $COLOR_05)
+COLOR_06=$(gnome_color $COLOR_06)
+COLOR_07=$(gnome_color $COLOR_07)
+COLOR_08=$(gnome_color $COLOR_08)
+
+COLOR_09=$(gnome_color $COLOR_09)
+COLOR_10=$(gnome_color $COLOR_10)
+COLOR_11=$(gnome_color $COLOR_11)
+COLOR_12=$(gnome_color $COLOR_12)
+COLOR_13=$(gnome_color $COLOR_13)
+COLOR_14=$(gnome_color $COLOR_14)
+COLOR_15=$(gnome_color $COLOR_15)
+COLOR_16=$(gnome_color $COLOR_16)
+
+
+
+# |
+# | Apply Variables
+# | ===========================================
+
+gconftool-2 --set /apps/gnome-terminal/profiles/Default/use_theme_background --type bool false
+gconftool-2 --set /apps/gnome-terminal/profiles/Default/use_theme_colors --type bool false
+gconftool-2 -s -t string /apps/gnome-terminal/profiles/Default/background_color "${BACKGROUD_COLOR}"
+gconftool-2 -s -t string /apps/gnome-terminal/profiles/Default/foreground_color "${FOREGROUND_COLOR}"
+gconftool-2 -s -t string /apps/gnome-terminal/profiles/Default/palette "${COLOR_01}:${COLOR_02}:${COLOR_03}:${COLOR_04}:${COLOR_05}:${COLOR_06}:${COLOR_07}:${COLOR_08}:${COLOR_09}:${COLOR_10}:${COLOR_11}:${COLOR_12}:${COLOR_13}:${COLOR_14}:${COLOR_15}:${COLOR_16}"
