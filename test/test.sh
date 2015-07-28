b3c0de5eb11ef987b00682e4472074b9b949bdba
Mayccoll <mayccoll@gmail.com>
2015-07-28 12:06:02 -0500

    Test file include upload

---
+++ test/test.sh
@@ -0,0 +1,225 @@
+#!/bin/bash
+
+
+# Reset color
+RS="\e[0m"
+# Basic Colors
+BLACK="\e[0;30m"
+RED="\e[0;31m"
+GREEN="\e[0;32m"
+YELLOW="\e[0;33m"
+BLUE="\e[0;34m"
+PURPLE="\e[0;35m"
+CYAN="\e[0;36m"
+WHITE="\e[0;37m"
+
+
+function_message_title () {
+    echo ""
+    echo ""
+    echo ""
+    echo ""
+    echo ""
+    echo -e "${CYAN}"
+    echo -e "# | ::::::::::::::::::::::::::::::::::::::::::::: | #"
+    echo -e "# |      ${RS} $1 ${CYAN}"
+    echo -e "# | ::::::::::::::::::::::::::::::::::::::::::::: | #"
+    echo -e "${RS}"
+}
+
+
+color () {
+    # echo ""
+    #black
+    echo -e "\e[0;30m ███ *** AaBbCs ---  ███ \\e[0m   ---> Color 01 0;30m"
+    #red
+    echo -e "\e[0;31m ███ *** AaBbCs ---  ███ \\e[0m   ---> Color 02 0;31m"
+    #green
+    echo -e "\e[0;32m ███ *** AaBbCs ---  ███ \\e[0m   ---> Color 03 0;32m"
+    #yellow
+    echo -e "\e[0;33m ███ *** AaBbCs ---  ███ \\e[0m   ---> Color 04 0;33m"
+    #blue
+    echo -e "\e[0;34m ███ *** AaBbCs ---  ███ \\e[0m   ---> Color 05 0;34m"
+    #purple
+    echo -e "\e[0;35m ███ *** AaBbCs ---  ███ \\e[0m   ---> Color 06 0;35m"
+    #cyan
+    echo -e "\e[0;36m ███ *** AaBbCs ---  ███ \\e[0m   ---> Color 07 0;36m"
+    #white
+    echo -e "\e[0;37m ███ *** AaBbCs ---  ███ \\e[0m   ---> Color 08 0;37m"
+    echo ""
+    #black
+    echo -e "\e[1;30m ███ *** AaBbCs ---  ███ \\e[0m   ---> Color 09 1;30m"
+    #red
+    echo -e "\e[1;31m ███ *** AaBbCs ---  ███ \\e[0m   ---> Color 10 1;31m"
+    #green
+    echo -e "\e[1;32m ███ *** AaBbCs ---  ███ \\e[0m   ---> Color 11 1;32m"
+    #yellow
+    echo -e "\e[1;33m ███ *** AaBbCs ---  ███ \\e[0m   ---> Color 12 1;33m"
+    #blue
+    echo -e "\e[1;34m ███ *** AaBbCs ---  ███ \\e[0m   ---> Color 13 1;34m"
+    #purple
+    echo -e "\e[1;35m ███ *** AaBbCs ---  ███ \\e[0m   ---> Color 14 1;35m"
+    #cyan
+    echo -e "\e[1;36m ███ *** AaBbCs ---  ███ \\e[0m   ---> Color 15 1;36m"
+    #white
+    echo -e "\e[1;37m ███ *** AaBbCs ---  ███ \\e[0m   ---> Color 16 1;37m"
+    echo ""
+    echo -e "\e[0;30m█████\\e[0m\e[0;31m█████\\e[0m\e[0;32m█████\\e[0m\e[0;33m█████\\e[0m\e[0;34m█████\\e[0m\e[0;35m█████\\e[0m\e[0;36m█████\\e[0m\e[0;37m█████\\e[0m"
+    echo -e "\e[0m\e[1;30m█████\\e[0m\e[1;31m█████\\e[0m\e[1;32m█████\\e[0m\e[1;33m█████\\e[0m\e[1;34m█████\\e[0m\e[1;35m█████\\e[0m\e[1;36m█████\\e[0m\e[1;37m█████\\e[0m"
+}
+
+
+function_message_title ' Theme Bim '
+wget -O xt  http://git.io/-t0lXg && chmod +x xt && ./xt && rm xt
+color
+sleep 1
+
+
+function_message_title ' Theme Aco '
+wget -O xt  http://git.io/0l4A5w && chmod +x xt && ./xt && rm xt
+color
+sleep 1
+
+
+function_message_title ' Theme Nep '
+wget -O xt  http://git.io/6hJsiw && chmod +x xt && ./xt && rm xt
+color
+sleep 1
+
+
+function_message_title ' Theme Miu '
+wget -O xt  http://git.io/b-eZVQ && chmod +x xt && ./xt && rm xt
+color
+sleep 1
+
+
+function_message_title ' Theme Elio '
+wget -O xt  http://git.io/bCVhIA && chmod +x xt && ./xt && rm xt
+color
+sleep 1
+
+
+function_message_title ' Theme Cai '
+wget -O xt  http://git.io/ECkzjg && chmod +x xt && ./xt && rm xt
+color
+sleep 1
+
+
+function_message_title ' Theme Elementary '
+wget -O xt  http://git.io/fXU8Jg && chmod +x xt && ./xt && rm xt
+color
+sleep 1
+
+
+function_message_title ' Theme Mar '
+wget -O xt  http://git.io/g749-Q && chmod +x xt && ./xt && rm xt
+color
+sleep 1
+
+
+function_message_title ' Theme Ura '
+wget -O xt  http://git.io/GUFFsQ && chmod +x xt && ./xt && rm xt
+color
+sleep 1
+
+
+function_message_title ' Theme Sat '
+wget -O xt  http://git.io/luv_2A && chmod +x xt && ./xt && rm xt
+color
+sleep 1
+
+
+function_message_title ' Theme Aci '
+wget -O xt  http://git.io/mGzs5Q && chmod +x xt && ./xt && rm xt
+color
+sleep 1
+
+
+function_message_title ' Theme Azu '
+wget -O xt  http://git.io/pDU8aQ && chmod +x xt && ./xt && rm xt
+color
+sleep 1
+
+
+function_message_title ' Theme Jup '
+wget -O xt  http://git.io/PE0xXw && chmod +x xt && ./xt && rm xt
+color
+sleep 1
+
+
+function_message_title ' Theme Hybrid '
+wget -O xt  http://git.io/PWgNtA && chmod +x xt && ./xt && rm xt
+color
+sleep 1
+
+
+function_message_title ' Theme Elic '
+wget -O xt  http://git.io/sSSshQ && chmod +x xt && ./xt && rm xt
+color
+sleep 1
+
+
+function_message_title ' Theme Shel '
+wget -O xt  http://git.io/TF6KDA && chmod +x xt && ./xt && rm xt
+color
+sleep 1
+
+
+function_message_title ' Theme Solarized Light '
+wget -O xt  http://git.io/vvjFb && chmod +x xt && ./xt && rm xt
+color
+sleep 1
+
+
+function_message_title ' Theme Solarized Dark '
+wget -O xt  http://git.io/vvjFH && chmod +x xt && ./xt && rm xt
+color
+sleep 1
+
+
+function_message_title ' Theme Monokai Dark '
+wget -O xt  http://git.io/vvjFK && chmod +x xt && ./xt && rm xt
+color
+sleep 1
+
+
+function_message_title ' Theme Freya '
+wget -O xt  http://git.io/vvmPj && chmod +x xt && ./xt && rm xt
+color
+sleep 1
+
+
+function_message_title ' Theme Tin '
+wget -O xt  http://git.io/W5dkrA && chmod +x xt && ./xt && rm xt
+color
+sleep 1
+
+
+function_message_title ' Theme Pali '
+wget -O xt  http://git.io/xh3YKA && chmod +x xt && ./xt && rm xt
+color
+sleep 1
+
+
+function_message_title ' Theme Vag '
+wget -O xt  http://git.io/xwkzlg && chmod +x xt && ./xt && rm xt
+color
+sleep 1
+
+
+function_message_title ' Theme Dracula '
+wget -O xt  http://git.io/vTBJC && chmod +x xt && ./xt && rm xt
+color
+sleep 1
+
+
+function_message_title ' Theme Chalk '
+wget -O xt  http://git.io/vmKFA && chmod +x xt && ./xt && rm xt
+color
+sleep 1
+
+
+function_message_title ' Theme Flat '
+wget -O xt  http://git.io/vvppo && chmod +x xt && ./xt && rm xt
+color
+sleep 1
