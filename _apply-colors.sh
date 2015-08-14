c67524203b131af46dc261f5c2ca99189013eda4
Mayccoll <mayccoll@gmail.com>
2015-08-14 12:01:15 -0500

    function is added to see the colors after installed

--- _apply-colors.sh
+++ _apply-colors.sh
@@ -60,7 +60,9 @@ else
 
 fi
 
-
+# |
+# | View color scheme in terminal
+# | ===========================================
 function gogh_colors () {
     echo ""
     echo -e "\e[0;30m█████\\e[0m\e[0;31m█████\\e[0m\e[0;32m█████\\e[0m\e[0;33m█████\\e[0m\e[0;34m█████\\e[0m\e[0;35m█████\\e[0m\e[0;36m█████\\e[0m\e[0;37m█████\\e[0m"
