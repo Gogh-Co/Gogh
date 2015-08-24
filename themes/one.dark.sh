b55cbaa03c3e54126e0214abe19a40acddb41439
Rafael Czerwinski <RafaelCz@users.noreply.github.com>
2015-08-24 23:08:18 +0200

    One Dark & One Light

---
+++ themes/one.dark.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# ====================CONFIG THIS =============================== #
+COLOR_01="#000000"
+COLOR_02="#E06C75"
+COLOR_03="#98C379"
+COLOR_04="#D19A66"
+COLOR_05="#61AFEF"
+COLOR_06="#C678DD"
+COLOR_07="#56B6C2"
+COLOR_08="#ABB2BF"
+
+COLOR_09="#5C6370"
+COLOR_10="#E06C75"
+COLOR_11="#98C379"
+COLOR_12="#D19A66"
+COLOR_13="#61AFEF"
+COLOR_14="#C678DD"
+COLOR_15="#56B6C2"
+COLOR_16="#FFFEFE"
+
+BACKGROUND_COLOR="#1E2127"
+FOREGROUND_COLOR="#5C6370"
+CURSOR_COLOR="#5C6370"
+# =============================================================== #
+
+# |
+# | Apply Colors
+# | ===========================================
+source <(wget  -O - http://git.io/vY8Qq)
