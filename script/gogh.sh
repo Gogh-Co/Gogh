aa8065dd6c19fb932bfd7e1c4221070d79943c29
Jhonny Moreira <xDeniedAccess@gmail.com>
2016-07-06 05:15:38 -0300

    Create a profile with the theme name
    The change used the issue #38 as reference, considering the Linux
    Mint problem with profile names containing spaces.

--- script/gogh.sh
+++ script/gogh.sh
@@ -51,122 +51,122 @@ read NUM
 case $NUM in
     0) echo '0 is not in OPTION' ;;
 
-    1 | 01) echo 'Thme: aci     '
-    wget -O xt  http://git.io/v3Dlm && chmod +x xt && ./xt && rm xt;;
+    1 | 01) echo 'Theme: aci'
+    export {PROFILE_NAME,PROFILE_SLUG}='aci' && wget -O xt http://git.io/v3Dlm && chmod +x xt && ./xt && rm xt;;
 
-    2 | 02) echo 'Thme: aco     '
-    wget -O xt  http://git.io/v3Dll && chmod +x xt && ./xt && rm xt;;
+    2 | 02) echo 'Theme: aco'
+    export {PROFILE_NAME,PROFILE_SLUG}='aco' && wget -O xt http://git.io/v3Dll && chmod +x xt && ./xt && rm xt;;
 
-    3 | 03) echo 'Thme: azu     '
-    wget -O xt  http://git.io/v3Dlz && chmod +x xt && ./xt && rm xt;;
+    3 | 03) echo 'Theme: azu'
+    export {PROFILE_NAME,PROFILE_SLUG}='azu' && wget -O xt http://git.io/v3Dlz && chmod +x xt && ./xt && rm xt;;
 
-    4 | 04) echo 'Thme: bim     '
-    wget -O xt  http://git.io/v3Dlr && chmod +x xt && ./xt && rm xt;;
+    4 | 04) echo 'Theme: bim'
+    export {PROFILE_NAME,PROFILE_SLUG}='bim' && wget -O xt http://git.io/v3Dlr && chmod +x xt && ./xt && rm xt;;
 
-    5 | 05) echo 'Thme: cai     '
-    wget -O xt  http://git.io/v3Dli && chmod +x xt && ./xt && rm xt;;
+    5 | 05) echo 'Theme: cai'
+    export {PROFILE_NAME,PROFILE_SLUG}='cai' && wget -O xt http://git.io/v3Dli && chmod +x xt && ./xt && rm xt;;
 
-    6 | 06) echo 'Thme: Chalk     '
-    wget -O xt  http://git.io/v3Dlb && chmod +x xt && ./xt && rm xt;;
+    6 | 06) echo 'Theme: Chalk'
+    export {PROFILE_NAME,PROFILE_SLUG}='Chalk' && wget -O xt http://git.io/v3Dlb && chmod +x xt && ./xt && rm xt;;
 
-    7 | 07) echo 'Thme: Dracula     '
-    wget -O xt  http://git.io/v3D8e && chmod +x xt && ./xt && rm xt;;
+    7 | 07) echo 'Theme: Dracula'
+    export {PROFILE_NAME,PROFILE_SLUG}='Dracula' && wget -O xt http://git.io/v3D8e && chmod +x xt && ./xt && rm xt;;
 
-    8 | 08) echo 'Thme: elementary     '
-    wget -O xt  http://git.io/v3D8R && chmod +x xt && ./xt && rm xt;;
+    8 | 08) echo 'Theme: elementary'
+    export {PROFILE_NAME,PROFILE_SLUG}='elementary' && wget -O xt http://git.io/v3D8R && chmod +x xt && ./xt && rm xt;;
 
-    9 | 09) echo 'Thme: elic     '
-    wget -O xt  http://git.io/v3D82 && chmod +x xt && ./xt && rm xt;;
+    9 | 09) echo 'Theme: elic'
+    export {PROFILE_NAME,PROFILE_SLUG}='elic' && wget -O xt http://git.io/v3D82 && chmod +x xt && ./xt && rm xt;;
 
-    10) echo 'Thme: elio     '
-    wget -O xt  http://git.io/v3D44 && chmod +x xt && ./xt && rm xt;;
+    10) echo 'Theme: elio'
+    export {PROFILE_NAME,PROFILE_SLUG}='elio' && wget -O xt http://git.io/v3D44 && chmod +x xt && ./xt && rm xt;;
 
-    11) echo 'Thme: Flat     '
-    wget -O xt  http://git.io/v3D4z && chmod +x xt && ./xt && rm xt;;
+    11) echo 'Theme: Flat'
+    export {PROFILE_NAME,PROFILE_SLUG}='Flat' && wget -O xt http://git.io/v3D4z && chmod +x xt && ./xt && rm xt;;
 
-    12) echo 'Thme: Freya     '
-    wget -O xt  http://git.io/v3D4o && chmod +x xt && ./xt && rm xt;;
+    12) echo 'Theme: Freya'
+    export {PROFILE_NAME,PROFILE_SLUG}='Freya' && wget -O xt http://git.io/v3D4o && chmod +x xt && ./xt && rm xt;;
 
-    13) echo 'Thme: Hemisu Dark     '
-    wget -O xt  http://git.io/v3D4M && chmod +x xt && ./xt && rm xt;;
+    13) echo 'Theme: Hemisu Dark'
+    export {PROFILE_NAME,PROFILE_SLUG}='HemisuDark' && wget -O xt http://git.io/v3D4M && chmod +x xt && ./xt && rm xt;;
 
-    14) echo 'Thme: Hemisu Light     '
-    wget -O xt  http://git.io/v3D49 && chmod +x xt && ./xt && rm xt;;
+    14) echo 'Theme: Hemisu Light'
+    export {PROFILE_NAME,PROFILE_SLUG}='HemisuLight' && wget -O xt http://git.io/v3D49 && chmod +x xt && ./xt && rm xt;;
 
-    15) echo 'Thme: Hybrid     '
-    wget -O xt  http://git.io/v3D4d && chmod +x xt && ./xt && rm xt;;
+    15) echo 'Theme: Hybrid'
+    export {PROFILE_NAME,PROFILE_SLUG}='Hybrid' && wget -O xt http://git.io/v3D4d && chmod +x xt && ./xt && rm xt;;
 
-    16) echo 'Thme: jup     '
-    wget -O xt  http://git.io/v3D4N && chmod +x xt && ./xt && rm xt;;
+    16) echo 'Theme: jup'
+    export {PROFILE_NAME,PROFILE_SLUG}='jup' && wget -O xt http://git.io/v3D4N && chmod +x xt && ./xt && rm xt;;
 
-    17) echo 'Thme: mar     '
-    wget -O xt  http://git.io/v3DBe && chmod +x xt && ./xt && rm xt;;
+    17) echo 'Theme: mar'
+    export {PROFILE_NAME,PROFILE_SLUG}='mar' && wget -O xt http://git.io/v3DBe && chmod +x xt && ./xt && rm xt;;
 
-    18) echo 'Thme: Material Colors     '
-    wget -O xt  http://git.io/v3DBv && chmod +x xt && ./xt && rm xt;;
+    18) echo 'Theme: Material Colors'
+    export {PROFILE_NAME,PROFILE_SLUG}='MaterialColors' && wget -O xt http://git.io/v3DBv && chmod +x xt && ./xt && rm xt;;
 
-    19) echo 'Thme: miu     '
-    wget -O xt  http://git.io/v3DBT && chmod +x xt && ./xt && rm xt;;
+    19) echo 'Theme: miu'
+    export {PROFILE_NAME,PROFILE_SLUG}='miu' && wget -O xt http://git.io/v3DBT && chmod +x xt && ./xt && rm xt;;
 
-    20) echo 'Thme: Monokai Dark     '
-    wget -O xt  http://git.io/v3DBO && chmod +x xt && ./xt && rm xt;;
+    20) echo 'Theme: Monokai Dark'
+    export {PROFILE_NAME,PROFILE_SLUG}='MonokaiDark' && wget -O xt http://git.io/v3DBO && chmod +x xt && ./xt && rm xt;;
 
-    21) echo 'Thme: nep     '
-    wget -O xt  http://git.io/v3DBB && chmod +x xt && ./xt && rm xt;;
+    21) echo 'Theme: nep'
+    export {PROFILE_NAME,PROFILE_SLUG}='nep' && wget -O xt http://git.io/v3DBB && chmod +x xt && ./xt && rm xt;;
 
-    22) echo 'Thme: Ocean Dark     '
-    wget -O xt  http://git.io/v3DBz && chmod +x xt && ./xt && rm xt;;
+    22) echo 'Theme: Ocean Dark'
+    export {PROFILE_NAME,PROFILE_SLUG}='OceanDark' && wget -O xt http://git.io/v3DBz && chmod +x xt && ./xt && rm xt;;
 
-    23) echo 'Thme: One Dark     $ '
-    wget -O xt  http://git.io/vs7Ut && chmod +x xt && ./xt && rm xt;;
+    23) echo 'Theme: One Dark     $ '
+    export {PROFILE_NAME,PROFILE_SLUG}='OneDark' && wget -O xt http://git.io/vs7Ut && chmod +x xt && ./xt && rm xt;;
 
-    24) echo 'Thme: One Light     '
-    wget -O xt  http://git.io/vs7UQ && chmod +x xt && ./xt && rm xt;;
+    24) echo 'Theme: One Light'
+    export {PROFILE_NAME,PROFILE_SLUG}='OneLight' && wget -O xt http://git.io/vs7UQ && chmod +x xt && ./xt && rm xt;;
 
-    25) echo 'Thme: pali     '
-    wget -O xt  http://git.io/v3DBV && chmod +x xt && ./xt && rm xt;;
+    25) echo 'Theme: pali'
+    export {PROFILE_NAME,PROFILE_SLUG}='pali' && wget -O xt http://git.io/v3DBV && chmod +x xt && ./xt && rm xt;;
 
-    26) echo 'Thme: Peppermint     '
-    wget -O xt  http://git.io/v3DBK && chmod +x xt && ./xt && rm xt;;
+    26) echo 'Theme: Peppermint'
+    export {PROFILE_NAME,PROFILE_SLUG}='Peppermint' && wget -O xt http://git.io/v3DBK && chmod +x xt && ./xt && rm xt;;
 
-    27) echo 'Thme: sat     '
-    wget -O xt  http://git.io/v3DBP && chmod +x xt && ./xt && rm xt;;
+    27) echo 'Theme: sat'
+    export {PROFILE_NAME,PROFILE_SLUG}='sat' && wget -O xt http://git.io/v3DBP && chmod +x xt && ./xt && rm xt;;
 
-    28) echo 'Thme: shel     '
-    wget -O xt  http://git.io/v3DBM && chmod +x xt && ./xt && rm xt;;
+    28) echo 'Theme: shel'
+    export {PROFILE_NAME,PROFILE_SLUG}='shel' && wget -O xt http://git.io/v3DBM && chmod +x xt && ./xt && rm xt;;
 
-    29) echo 'Thme: Smyck     '
-    wget -O xt  http://git.io/v3DB9 && chmod +x xt && ./xt && rm xt;;
+    29) echo 'Theme: Smyck'
+    export {PROFILE_NAME,PROFILE_SLUG}='Smyck' && wget -O xt http://git.io/v3DB9 && chmod +x xt && ./xt && rm xt;;
 
-    30) echo 'Thme: Solarized Dark     '
-    wget -O xt  http://git.io/v3DBQ && chmod +x xt && ./xt && rm xt;;
+    30) echo 'Theme: Solarized Dark'
+    export {PROFILE_NAME,PROFILE_SLUG}='SolarizedDark' && wget -O xt http://git.io/v3DBQ && chmod +x xt && ./xt && rm xt;;
 
-    31) echo 'Thme: Solarized Light     '
-    wget -O xt  http://git.io/v3DB5 && chmod +x xt && ./xt && rm xt;;
+    31) echo 'Theme: Solarized Light'
+    export {PROFILE_NAME,PROFILE_SLUG}='SolarizedLight' && wget -O xt http://git.io/v3DB5 && chmod +x xt && ./xt && rm xt;;
 
-    32) echo 'Thme: tin     '
-    wget -O xt  http://git.io/v3DBF && chmod +x xt && ./xt && rm xt;;
+    32) echo 'Theme: tin'
+    export {PROFILE_NAME,PROFILE_SLUG}='tin' && wget -O xt http://git.io/v3DBF && chmod +x xt && ./xt && rm xt;;
 
-    33) echo 'Thme: Tomorrow     '
-    wget -O xt  http://git.io/v3DRc && chmod +x xt && ./xt && rm xt;;
+    33) echo 'Theme: Tomorrow'
+    export {PROFILE_NAME,PROFILE_SLUG}='Tomorrow' && wget -O xt http://git.io/v3DRc && chmod +x xt && ./xt && rm xt;;
 
-    34) echo 'Thme: Tomorrow Night     '
-    wget -O xt  http://git.io/v3DRs && chmod +x xt && ./xt && rm xt;;
+    34) echo 'Theme: Tomorrow Night'
+    export {PROFILE_NAME,PROFILE_SLUG}='TomorrowNight' && wget -O xt http://git.io/v3DRs && chmod +x xt && ./xt && rm xt;;
 
-    35) echo 'Thme: Tomorrow Night Blue     '
-    wget -O xt  http://git.io/v3DBj && chmod +x xt && ./xt && rm xt;;
+    35) echo 'Theme: Tomorrow Night Blue'
+    export {PROFILE_NAME,PROFILE_SLUG}='TomorrowNightBlue' && wget -O xt http://git.io/v3DBj && chmod +x xt && ./xt && rm xt;;
 
-    36) echo 'Thme: Tomorrow Night Bright     '
-    wget -O xt  http://git.io/v3DRJ && chmod +x xt && ./xt && rm xt;;
+    36) echo 'Theme: Tomorrow Night Bright'
+    export {PROFILE_NAME,PROFILE_SLUG}='TomorrowNightBright' && wget -O xt http://git.io/v3DRJ && chmod +x xt && ./xt && rm xt;;
 
-    37) echo 'Thme: Tomorrow Night Eighties     '
-    wget -O xt  http://git.io/v3DRI && chmod +x xt && ./xt && rm xt;;
+    37) echo 'Theme: Tomorrow Night Eighties'
+    export {PROFILE_NAME,PROFILE_SLUG}='TomorrowNightEighties' && wget -O xt http://git.io/v3DRI && chmod +x xt && ./xt && rm xt;;
 
-    38) echo 'Thme: ura     '
-    wget -O xt  http://git.io/v3DRB && chmod +x xt && ./xt && rm xt;;
+    38) echo 'Theme: ura'
+    export {PROFILE_NAME,PROFILE_SLUG}='ura' && wget -O xt http://git.io/v3DRB && chmod +x xt && ./xt && rm xt;;
 
-    39) echo 'Thme: vag     '
-    wget -O xt  http://git.io/v3DR0 && chmod +x xt && ./xt && rm xt;;
+    39) echo 'Theme: vag'
+    export {PROFILE_NAME,PROFILE_SLUG}='vag' && wget -O xt http://git.io/v3DR0 && chmod +x xt && ./xt && rm xt;;
 
     *) echo 'INVALID OPTION!' ;;
         esac
