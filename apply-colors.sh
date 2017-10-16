a953f1662abf46b7bc4022365b5a05559c4bed4b
Dimitri Bohlender <bohlender@embedded.rwth-aachen.de>
2017-10-16 17:00:55 +0200

    Notes regarding the use of gconf

--- apply-colors.sh
+++ apply-colors.sh
@@ -63,6 +63,8 @@ if [[ $TERMINAL =~ "guake" ]]; then
     # |
     # | Applying values if string contains guake.main
     # | =============================================
+	# Note: Guake still uses gconf but plans to support dconf/gsettings when reaching 1.0.0.
+	#       See notes for 0.8.1 in https://github.com/Guake/guake/blob/master/NEWS.
 	gconftool-2 -s -t string /apps/guake/style/background/color "${BACKGROUND_COLOR}"
 	gconftool-2 -s -t string /apps/guake/style/font/color "${FOREGROUND_COLOR}"
 	gconftool-2 -s -t string /apps/guake/style/font/palette "${COLOR_01}:${COLOR_02}:${COLOR_03}:${COLOR_04}:${COLOR_05}:${COLOR_06}:${COLOR_07}:${COLOR_08}:${COLOR_09}:${COLOR_10}:${COLOR_11}:${COLOR_12}:${COLOR_13}:${COLOR_14}:${COLOR_15}:${COLOR_16}"
