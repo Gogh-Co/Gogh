076aff94c1bb0018ba32956d87fa79ea2a3eb0a1
Miguel D <miguelq@rabinoviconline.com>
2019-10-09 21:48:58 -0500

    Fix Gotham colors

--- themes/gotham.sh
+++ themes/gotham.sh
@@ -1,26 +1,26 @@
 #!/usr/bin/env bash
 
 # ====================CONFIG THIS =============================== #
-export COLOR_01="0a/0f/14" # Base 00 - Black
-export COLOR_02="c3/30/27" # Base 08 - Red
-export COLOR_03="26/a9/8b" # Base 0B - Green
-export COLOR_04="ed/b5/4b" # Base 0A - Yellow
-export COLOR_05="19/54/65" # Base 0D - Blue
-export COLOR_06="4e/51/65" # Base 0E - Magenta
-export COLOR_07="33/85/9d" # Base 0C - Cyan
-export COLOR_08="98/d1/ce" # Base 05 - White
+export COLOR_01="#0a0f14" # Base 00 - Black
+export COLOR_02="#c33027" # Base 08 - Red
+export COLOR_03="#26a98b" # Base 0B - Green
+export COLOR_04="#edb54b" # Base 0A - Yellow
+export COLOR_05="#195465" # Base 0D - Blue
+export COLOR_06="#4e5165" # Base 0E - Magenta
+export COLOR_07="#33859d" # Base 0C - Cyan
+export COLOR_08="#98d1ce" # Base 05 - White
 
-export COLOR_09="10/15/1b" # Base 03 - Bright Black
-export COLOR_10="d2/69/39" # Base 08 - Bright Red
-export COLOR_11="08/1f/2d" # Base 0B - Bright Green
-export COLOR_12="24/53/61" # Base 0A - Bright Yellow
-export COLOR_13="09/37/48" # Base 0D - Bright Blue
-export COLOR_14="88/8b/a5" # Base 0E - Bright Magenta
-export COLOR_15="59/9c/aa" # Base 0C - Bright Cyan
-export COLOR_16="d3/eb/e9" # Base 07 - Bright White
+export COLOR_09="#10151b" # Base 03 - Bright Black
+export COLOR_10="#d26939" # Base 08 - Bright Red
+export COLOR_11="#081f2d" # Base 0B - Bright Green
+export COLOR_12="#245361" # Base 0A - Bright Yellow
+export COLOR_13="#093748" # Base 0D - Bright Blue
+export COLOR_14="#888ba5" # Base 0E - Bright Magenta
+export COLOR_15="#599caa" # Base 0C - Bright Cyan
+export COLOR_16="#d3ebe9" # Base 07 - Bright White
 
-export FOREGROUND_COLOR="98/d1/ce" # Base 05
-export BACKGROUND_COLOR="0a/0f/14" # Base 00
+export FOREGROUND_COLOR="#98d1ce" # Base 05
+export BACKGROUND_COLOR="#0a0f14" # Base 00
 export CURSOR_COLOR="$FOREGROUND_COLOR" # Cursor
 export PROFILE_NAME="gotham"
 # =============================================================== #
