8502d3a43d5110faed075ff32776fa4218bae5a3
Miguel D <miguelq@rabinoviconline.com>
2020-03-05 23:29:02 -0500

    add VUE for landing page

---
+++ data/themes.js
@@ -0,0 +1,856 @@
+var $themes = [{
+    "name": "3024 Day",
+    "image": "/images/themes/3024-day.jpg",
+    "filters": "light bg-white"
+  },
+  {
+    "name": "3024 Night",
+    "image": "/images/themes/3024-night.jpg",
+    "filters": "dark bg-brown"
+  },
+  {
+    "name": "Aci",
+    "image": "/images/themes/aci.jpg",
+    "filters": "dark bg-blue"
+  },
+  {
+    "name": "Aco",
+    "image": "/images/themes/aco.jpg",
+    "filters": "dark bg-brown"
+  },
+  {
+    "name": "Adventure Time",
+    "image": "/images/themes/adventure-time.jpg",
+    "filters": "dark bg-blue"
+  },
+  {
+    "name": "Afterglow",
+    "image": "/images/themes/afterglow.jpg",
+    "filters": "dark bg-grey"
+  },
+  {
+    "name": "Alien Blood",
+    "image": "/images/themes/alien-blood.jpg",
+    "filters": "dark bg-green"
+  },
+  {
+    "name": "Argonaut",
+    "image": "/images/themes/argonaut.jpg",
+    "filters": "dark bg-black"
+  },
+  {
+    "name": "Arthur",
+    "image": "/images/themes/arthur.jpg",
+    "filters": "dark bg-grey"
+  },
+  {
+    "name": "Atom",
+    "image": "/images/themes/atom.jpg",
+    "filters": "dark bg-grey"
+  },
+  {
+    "name": "Azu",
+    "image": "/images/themes/azu.jpg",
+    "filters": "dark bg-black"
+  },
+  {
+    "name": "Belafonte Day",
+    "image": "/images/themes/belafonte-day.jpg",
+    "filters": "light bg-beich"
+  },
+  {
+    "name": "Belafonte Night",
+    "image": "/images/themes/belafonte-night.jpg",
+    "filters": "dark bg-purple"
+  },
+  {
+    "name": "Bim",
+    "image": "/images/themes/bim.jpg",
+    "filters": "dark bg-blue"
+  },
+  {
+    "name": "Birds Of Paradise",
+    "image": "/images/themes/birds-of-paradise.jpg",
+    "filters": "dark bg-brown"
+  },
+  {
+    "name": "Blazer",
+    "image": "/images/themes/blazer.jpg",
+    "filters": "dark bg-blue"
+  },
+  {
+    "name": "Borland",
+    "image": "/images/themes/borland.jpg",
+    "filters": "dark bg-blue"
+  },
+  {
+    "name": "Broadcast",
+    "image": "/images/themes/broadcast.jpg",
+    "filters": "dark bg-grey"
+  },
+  {
+    "name": "Brogrammer",
+    "image": "/images/themes/brogrammer.jpg",
+    "filters": "dark bg-black"
+  },
+  {
+    "name": "C64",
+    "image": "/images/themes/c64.jpg",
+    "filters": "dark bg-violet"
+  },
+  {
+    "name": "Cai",
+    "image": "/images/themes/cai.jpg",
+    "filters": "dark bg-blue"
+  },
+  {
+    "name": "Chalk",
+    "image": "/images/themes/chalk.jpg",
+    "filters": "dark bg-grey"
+  },
+  {
+    "name": "Chalkboard",
+    "image": "/images/themes/chalkboard.jpg",
+    "filters": "dark bg-grey"
+  },
+  {
+    "name": "Ciapre",
+    "image": "/images/themes/ciapre.jpg",
+    "filters": "dark bg-grey"
+  },
+  {
+    "name": "Clone Of Ubuntu",
+    "image": "/images/themes/clone-of-ubuntu.jpg",
+    "filters": "dark bg-purple"
+  },
+  {
+    "name": "CLRS",
+    "image": "/images/themes/clrs.jpg",
+    "filters": "light bg-white"
+  },
+  {
+    "name": "Cobalt Neon",
+    "image": "/images/themes/cobalt-neon.jpg",
+    "filters": "dark bg-blue"
+  },
+  {
+    "name": "Cobalt 2",
+    "image": "/images/themes/cobalt2.jpg",
+    "filters": "dark bg-blue"
+  },
+  {
+    "name": "Crayon Pony Fish",
+    "image": "/images/themes/crayon-pony-fish.jpg",
+    "filters": "dark bg-brown"
+  },
+  {
+    "name": "Dark Pastel",
+    "image": "/images/themes/dark-pastel.jpg",
+    "filters": "dark bg-black"
+  },
+  {
+    "name": "Darkside",
+    "image": "/images/themes/darkside.jpg",
+    "filters": "dark bg-grey"
+  },
+  {
+    "name": "Desert",
+    "image": "/images/themes/desert.jpg",
+    "filters": "dark bg-grey"
+  },
+  {
+    "name": "Dimmed Monokai",
+    "image": "/images/themes/dimmed-monokai.jpg",
+    "filters": "dark bg-grey"
+  },
+  {
+    "name": "Dracula",
+    "image": "/images/themes/dracula.jpg",
+    "filters": "dark bg-blue"
+  },
+  {
+    "name": "Earthsong",
+    "image": "/images/themes/earthsong.jpg",
+    "filters": "dark bg-brown"
+  },
+  {
+    "name": "Elemental",
+    "image": "/images/themes/elemental.jpg",
+    "filters": "dark bg-brown"
+  },
+  {
+    "name": "Elementary",
+    "image": "/images/themes/elementary.jpg",
+    "filters": "dark bg-black"
+  },
+  {
+    "name": "Elic",
+    "image": "/images/themes/elic.jpg",
+    "filters": "dark bg-brown"
+  },
+  {
+    "name": "Elio",
+    "image": "/images/themes/elio.jpg",
+    "filters": "dark bg-blue"
+  },
+  {
+    "name": "Espresso Libre",
+    "image": "/images/themes/espresso-libre.jpg",
+    "filters": "dark bg-brown"
+  },
+  {
+    "name": "Espresso",
+    "image": "/images/themes/espresso.jpg",
+    "filters": "dark bg-grey"
+  },
+  {
+    "name": "Fishtank",
+    "image": "/images/themes/fishtank.jpg",
+    "filters": "dark bg-grey"
+  },
+  {
+    "name": "Flat",
+    "image": "/images/themes/flat.jpg",
+    "filters": "dark bg-blue"
+  },
+  {
+    "name": "Flatland",
+    "image": "/images/themes/flatland.jpg",
+    "filters": "dark bg-grey"
+  },
+  {
+    "name": "Foxnightly",
+    "image": "/images/themes/foxnightly.jpg",
+    "filters": "dark bg-grey"
+  },
+  {
+    "name": "Freya",
+    "image": "/images/themes/freya.jpg",
+    "filters": "dark bg-grey"
+  },
+  {
+    "name": "Frontend Delight",
+    "image": "/images/themes/frontend-delight.jpg",
+    "filters": "dark bg-grey"
+  },
+  {
+    "name": "Frontend Fun Forrest",
+    "image": "/images/themes/frontend-fun-forrest.jpg",
+    "filters": "dark bg-brown"
+  },
+  {
+    "name": "Frontend Galaxy",
+    "image": "/images/themes/frontend-galaxy.jpg",
+    "filters": "dark bg-grey"
+  },
+  {
+    "name": "Github",
+    "image": "/images/themes/github.jpg",
+    "filters": "light bg-white"
+  },
+  {
+    "name": "Gotham",
+    "image": "/images/themes/gotham.jpg",
+    "filters": "dark bg-black"
+  },
+  {
+    "name": "Google Dark",
+    "image": "/images/themes/google-dark.jpg",
+    "filters": "dark bg-grey"
+  },
+  {
+    "name": "Google Light",
+    "image": "/images/themes/google-light.jpg",
+    "filters": "light bg-white"
+  },
+  {
+    "name": "Gooey",
+    "image": "/images/themes/gooey.jpg",
+    "filters": "dark bg-grey"
+  },
+  {
+    "name": "Grape",
+    "image": "/images/themes/grape.jpg",
+    "filters": "dark bg-blue"
+  },
+  {
+    "name": "Grass",
+    "image": "/images/themes/grass.jpg",
+    "filters": "dark bg-green"
+  },
+  {
+    "name": "Gruvbox Dark",
+    "image": "/images/themes/gruvbox-dark.jpg",
+    "filters": "dark bg-grey"
+  },
+  {
+    "name": "Gruvbox",
+    "image": "/images/themes/gruvbox.jpg",
+    "filters": "light bg-beich"
+  },
+  {
+    "name": "Hardcore",
+    "image": "/images/themes/hardcore.jpg",
+    "filters": "dark bg-black"
+  },
+  {
+    "name": "Harper",
+    "image": "/images/themes/harper.jpg",
+    "filters": "dark bg-black"
+  },
+  {
+    "name": "Hemisu Dark",
+    "image": "/images/themes/hemisu-dark.jpg",
+    "filters": "dark bg-black"
+  },
+  {
+    "name": "Hemisu Light",
+    "image": "/images/themes/hemisu-light.jpg",
+    "filters": "light bg-white"
+  },
+  {
+    "name": "Highway",
+    "image": "/images/themes/highway.jpg",
+    "filters": "dark bg-grey"
+  },
+  {
+    "name": "Hipster Green",
+    "image": "/images/themes/hipster-green.jpg",
+    "filters": "dark bg-green"
+  },
+  {
+    "name": "Homebrew",
+    "image": "/images/themes/homebrew.jpg",
+    "filters": "dark bg-black"
+  },
+  {
+    "name": "Hurtado",
+    "image": "/images/themes/hurtado.jpg",
+    "filters": "dark bg-black"
+  },
+  {
+    "name": "Hydrid",
+    "image": "/images/themes/Hybrid.jpg",
+    "filters": "dark bg-black"
+  },
+  {
+    "name": "Ic Green Ppl",
+    "image": "/images/themes/ic-green-ppl.jpg",
+    "filters": "dark bg-grey"
+  },
+  {
+    "name": "Ic Orange Ppl",
+    "image": "/images/themes/ic-orange-ppl.jpg",
+    "filters": "dark bg-grey"
+  },
+  {
+    "name": "Idle Toes",
+    "image": "/images/themes/idle-toes.jpg",
+    "filters": "dark bg-grey"
+  },
+  {
+    "name": "Ir Black",
+    "image": "/images/themes/ir-black.jpg",
+    "filters": "dark bg-black"
+  },
+  {
+    "name": "Jackie Brown",
+    "image": "/images/themes/jackie-brown.jpg",
+    "filters": "dark bg-brown"
+  },
+  {
+    "name": "Japanesque",
+    "image": "/images/themes/japanesque.jpg",
+    "filters": "dark bg-grey"
+  },
+  {
+    "name": "Jellybeans",
+    "image": "/images/themes/jellybeans.jpg",
+    "filters": "dark bg-black"
+  },
+  {
+    "name": "Jup",
+    "image": "/images/themes/jup.jpg",
+    "filters": "dark bg-grey"
+  },
+  {
+    "name": "Kibble",
+    "image": "/images/themes/kibble.jpg",
+    "filters": "dark bg-black"
+  },
+  {
+    "name": "kokuban",
+    "image": "/images/themes/kokuban.jpg",
+    "filters": "dark bg-black"
+  },
+  {
+    "name": "Later This Evening",
+    "image": "/images/themes/later-this-evening.jpg",
+    "filters": "dark bg-grey"
+  },
+  {
+    "name": "Lavandula",
+    "image": "/images/themes/lavandula.jpg",
+    "filters": "dark bg-violet"
+  },
+  {
+    "name": "Liquid Carbon",
+    "image": "/images/themes/liquid-carbon.jpg",
+    "filters": "dark bg-grey"
+  },
+  {
+    "name": "Liquid Carbon Transparent",
+    "image": "/images/themes/liquid-carbon-transparent.jpg",
+    "filters": "dark bg-black"
+  },
+  {
+    "name": "Maia",
+    "image": "/images/themes/maia.jpg",
+    "filters": "dark bg-grey"
+  },
+  {
+    "name": "Man Page",
+    "image": "/images/themes/man-page.jpg",
+    "filters": "light bg-beich"
+  },
+  {
+    "name": "Mar",
+    "image": "/images/themes/mar.jpg",
+    "filters": "light bg-white"
+  },
+  {
+    "name": "Material Colors",
+    "image": "/images/themes/material-colors.jpg",
+    "filters": "dark bg-grey"
+  },
+  {
+    "name": "Mathias",
+    "image": "/images/themes/mathias.jpg",
+    "filters": "dark bg-black"
+  },
+  {
+    "name": "Medallion",
+    "image": "/images/themes/medallion.jpg",
+    "filters": "dark bg-brown"
+  },
+  {
+    "name": "Misterioso",
+    "image": "/images/themes/misterioso.jpg",
+    "filters": "dark bg-grey"
+  },
+  {
+    "name": "Miu",
+    "image": "/images/themes/miu.jpg",
+    "filters": "dark bg-blue"
+  },
+  {
+    "name": "Molokai",
+    "image": "/images/themes/molokai.jpg",
+    "filters": "dark bg-grey"
+  },
+  {
+    "name": "Mona Lisa",
+    "image": "/images/themes/mona-lisa.jpg",
+    "filters": "dark bg-brown"
+  },
+  {
+    "name": "Monokai Dark",
+    "image": "/images/themes/monokai.dark.jpg",
+    "filters": "dark bg-brown"
+  },
+  {
+    "name": "Monokai Soda",
+    "image": "/images/themes/monokai-soda.jpg",
+    "filters": "dark bg-grey"
+  },
+  {
+    "name": "N0tch2k",
+    "image": "/images/themes/n0tch2k.jpg",
+    "filters": "dark bg-grey"
+  },
+  {
+    "name": "Neopolitan",
+    "image": "/images/themes/neopolitan.jpg",
+    "filters": "dark bg-brown"
+  },
+  {
+    "name": "Nep",
+    "image": "/images/themes/nep.jpg",
+    "filters": "dark bg-grey"
+  },
+  {
+    "name": "Neutron",
+    "image": "/images/themes/neutron.jpg",
+    "filters": "dark bg-grey"
+  },
+  {
+    "name": "Nightlion V1",
+    "image": "/images/themes/nightlion-v1.jpg",
+    "filters": "dark bg-black"
+  },
+  {
+    "name": "Nightlion V2",
+    "image": "/images/themes/nightlion-v2.jpg",
+    "filters": "dark bg-grey"
+  },
+  {
+    "name": "Nighty",
+    "image": "/images/themes/nighty.jpg",
+    "filters": "dark bg-grey"
+  },
+  {
+    "name": "Nord Light",
+    "image": "/images/themes/nord-light.jpg",
+    "filters": "light bg-white"
+  },
+  {
+    "name": "Nord",
+    "image": "/images/themes/nord.jpg",
+    "filters": "dark bg-grey"
+  },
+  {
+    "name": "Novel",
+    "image": "/images/themes/novel.jpg",
+    "filters": "light bg-beich"
+  },
+  {
+    "name": "Obsidian",
+    "image": "/images/themes/obsidian.jpg",
+    "filters": "dark bg-grey"
+  },
+  {
+    "name": "Ocean Dark",
+    "image": "/images/themes/ocean-dark.jpg",
+    "filters": "dark bg-blue"
+  },
+  {
+    "name": "Ocean",
+    "image": "/images/themes/ocean.jpg",
+    "filters": "dark bg-blue"
+  },
+  {
+    "name": "Oceanic Next",
+    "image": "/images/themes/oceanic-next.jpg",
+    "filters": "dark bg-grey"
+  },
+  {
+    "name": "Ollie",
+    "image": "/images/themes/ollie.jpg",
+    "filters": "dark bg-grey"
+  },
+  {
+    "name": "One Dark",
+    "image": "/images/themes/one.dark.jpg",
+    "filters": "dark bg-black"
+  },
+  {
+    "name": "One Half Black",
+    "image": "/images/themes/one-half-black.jpg",
+    "filters": "dark bg-black"
+  },
+  {
+    "name": "One Light",
+    "image": "/images/themes/one.light.jpg",
+    "filters": "light bg-white"
+  },
+  {
+    "name": "Pali",
+    "image": "/images/themes/pali.jpg",
+    "filters": "dark bg-blue"
+  },
+  {
+    "name": "Paraiso Dark",
+    "image": "/images/themes/paraiso-dark.jpg",
+    "filters": "dark bg-purple"
+  },
+  {
+    "name": "Paul Millr",
+    "image": "/images/themes/paul-millr.jpg",
+    "filters": "dark bg-black"
+  },
+  {
+    "name": "Pencil Dark",
+    "image": "/images/themes/pencil-dark.jpg",
+    "filters": "dark bg-grey"
+  },
+  {
+    "name": "Pencil Light",
+    "image": "/images/themes/pencil-light.jpg",
+    "filters": "light bg-white"
+  },
+  {
+    "name": "Peppermint",
+    "image": "/images/themes/peppermint.jpg",
+    "filters": "dark bg-black"
+  },
+  {
+    "name": "Pnevma",
+    "image": "/images/themes/pnevma.jpg",
+    "filters": "dark bg-grey"
+  },
+  {
+    "name": "Pro",
+    "image": "/images/themes/pro.jpg",
+    "filters": "dark bg-black"
+  },
+  {
+    "name": "Red Alert",
+    "image": "/images/themes/red-alert.jpg",
+    "filters": "dark bg-red"
+  },
+  {
+    "name": "Red Sands",
+    "image": "/images/themes/red-sands.jpg",
+    "filters": "dark bg-red"
+  },
+  {
+    "name": "Rippedcasts",
+    "image": "/images/themes/rippedcasts.jpg",
+    "filters": "dark bg-grey"
+  },
+  {
+    "name": "Royal",
+    "image": "/images/themes/royal.jpg",
+    "filters": "dark bg-violet"
+  },
+  {
+    "name": "Sat",
+    "image": "/images/themes/sat.jpg",
+    "filters": "dark bg-grey"
+  },
+  {
+    "name": "Sea Shells",
+    "image": "/images/themes/sea-shells.jpg",
+    "filters": "dark bg-grey"
+  },
+  {
+    "name": "Seafoam Pastel",
+    "image": "/images/themes/seafoam-pastel.jpg",
+    "filters": "dark bg-grey"
+  },
+  {
+    "name": "Seti",
+    "image": "/images/themes/seti.jpg",
+    "filters": "dark bg-black"
+  },
+  {
+    "name": "Shaman",
+    "image": "/images/themes/shaman.jpg",
+    "filters": "dark bg-black"
+  },
+  {
+    "name": "Shel",
+    "image": "/images/themes/shel.jpg",
+    "filters": "dark bg-brown"
+  },
+  {
+    "name": "Slate",
+    "image": "/images/themes/slate.jpg",
+    "filters": "dark bg-grey"
+  },
+  {
+    "name": "Smyck",
+    "image": "/images/themes/smyck.jpg",
+    "filters": "dark bg-grey"
+  },
+  {
+    "name": "Snazzy",
+    "image": "/images/themes/snazzy.jpg",
+    "filters": "dark bg-grey"
+  },
+  {
+    "name": "Soft Server",
+    "image": "/images/themes/soft-server.jpg",
+    "filters": "dark bg-grey"
+  },
+  {
+    "name": "Solarized Darcula",
+    "image": "/images/themes/solarized-darcula.jpg",
+    "filters": "dark bg-grey"
+  },
+  {
+    "name": "Solarized Dark Higher Contrast",
+    "image": "/images/themes/solarized-dark-higher-contrast.jpg",
+    "filters": "dark bg-blue"
+  },
+  {
+    "name": "Solarized Dark",
+    "image": "/images/themes/solarized.dark.jpg",
+    "filters": "dark bg-blue"
+  },
+  {
+    "name": "Solarized Light",
+    "image": "/images/themes/solarized.light.jpg",
+    "filters": "light bg-beich"
+  },
+  {
+    "name": "Spacedust",
+    "image": "/images/themes/spacedust.jpg",
+    "filters": "dark bg-blue"
+  },
+  {
+    "name": "Spacegray Eighties Dull",
+    "image": "/images/themes/spacegray-eighties-dull.jpg",
+    "filters": "dark bg-grey"
+  },
+  {
+    "name": "Spacegray Eighties",
+    "image": "/images/themes/spacegray-eighties.jpg",
+    "filters": "dark bg-grey"
+  },
+  {
+    "name": "Spacegray",
+    "image": "/images/themes/spacegray.jpg",
+    "filters": "dark bg-grey"
+  },
+  {
+    "name": "Spring",
+    "image": "/images/themes/spring.jpg",
+    "filters": "dark bg-grey"
+  },
+  {
+    "name": "Square",
+    "image": "/images/themes/square.jpg",
+    "filters": "dark bg-grey"
+  },
+  {
+    "name": "Srcery",
+    "image": "/images/themes/srcery.jpg",
+    "filters": "dark bg-grey"
+  },
+  {
+    "name": "Summer Pop",
+    "image": "/images/themes/summer-pop.jpg",
+    "filters": "dark bg-brown"
+  },
+  {
+    "name": "Sundried",
+    "image": "/images/themes/sundried.jpg",
+    "filters": "dark bg-brown"
+  },
+  {
+    "name": "Symphonic",
+    "image": "/images/themes/symphonic.jpg",
+    "filters": "dark bg-black"
+  },
+  {
+    "name": "Teerb",
+    "image": "/images/themes/teerb.jpg",
+    "filters": "dark bg-grey"
+  },
+  {
+    "name": "Terminal Basic",
+    "image": "/images/themes/terminal-basic.jpg",
+    "filters": "light bg-white"
+  },
+  {
+    "name": "Terminix Dark",
+    "image": "/images/themes/terminix-dark.jpg",
+    "filters": "dark bg-black"
+  },
+  {
+    "name": "Thayer Bright",
+    "image": "/images/themes/thayer-bright.jpg",
+    "filters": "dark bg-grey"
+  },
+  {
+    "name": "Tin",
+    "image": "/images/themes/tin.jpg",
+    "filters": "dark bg-grey"
+  },
+  {
+    "name": "Tomorrow Night Blue",
+    "image": "/images/themes/tomorrow-night-blue.jpg",
+    "filters": "dark bg-blue"
+  },
+  {
+    "name": "Tomorrow Night Bright",
+    "image": "/images/themes/tomorrow-night-bright.jpg",
+    "filters": "dark bg-black"
+  },
+  {
+    "name": "Tomorrow Night Eighties",
+    "image": "/images/themes/tomorrow-night-eighties.jpg",
+    "filters": "dark bg-grey"
+  },
+  {
+    "name": "Tomorrow Night",
+    "image": "/images/themes/tomorrow-night.jpg",
+    "filters": "dark bg-grey"
+  },
+  {
+    "name": "Tomorrow",
+    "image": "/images/themes/tomorrow.jpg",
+    "filters": "light bg-white"
+  },
+  {
+    "name": "Toy Chest",
+    "image": "/images/themes/toy-chest.jpg",
+    "filters": "dark bg-blue"
+  },
+  {
+    "name": "Treehouse",
+    "image": "/images/themes/treehouse.jpg",
+    "filters": "dark bg-brown"
+  },
+  {
+    "name": "Twilight",
+    "image": "/images/themes/twilight.jpg",
+    "filters": "dark bg-grey"
+  },
+  {
+    "name": "Ura",
+    "image": "/images/themes/ura.jpg",
+    "filters": "light bg-beich"
+  },
+  {
+    "name": "Urple",
+    "image": "/images/themes/urple.jpg",
+    "filters": "dark bg-purple"
+  },
+  {
+    "name": "Vag",
+    "image": "/images/themes/vag.jpg",
+    "filters": "dark bg-grey"
+  },
+  {
+    "name": "Vaughn",
+    "image": "/images/themes/vaughn.jpg",
+    "filters": "dark bg-violet"
+  },
+  {
+    "name": "Vibrant Ink",
+    "image": "/images/themes/vibrant-ink.jpg",
+    "filters": "dark bg-black"
+  },
+  {
+    "name": "Warm Neon",
+    "image": "/images/themes/warm-neon.jpg",
+    "filters": "dark bg-grey"
+  },
+  {
+    "name": "Wez",
+    "image": "/images/themes/wez.jpg",
+    "filters": "dark bg-black"
+  },
+  {
+    "name": "Wild Cherry",
+    "image": "/images/themes/wild-cherry.jpg",
+    "filters": "dark bg-purple"
+  },
+  {
+    "name": "Wombat",
+    "image": "/images/themes/wombat.jpg",
+    "filters": "dark bg-grey"
+  },
+  {
+    "name": "Wryan",
+    "image": "/images/themes/wryan.jpg",
+    "filters": "dark bg-grey"
+  },
+  {
+    "name": "Zenburn",
+    "image": "/images/themes/zenburn.jpg",
+    "filters": "dark bg-grey"
+  }
+]
