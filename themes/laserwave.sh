86fa7c93f95b40a13cd7921f176d1a7585af450d
averak <abe12@mccc.jp>
2021-02-19 09:36:12 +0900

    fix colors

--- themes/laserwave.sh
+++ themes/laserwave.sh
@@ -1,26 +1,26 @@
 #!/usr/bin/env bash
 
 # ====================CONFIG THIS =============================== #
-export COLOR_01="#303030"           # Black
+export COLOR_01="#39243A"           # Black
 export COLOR_02="#EB64B9"           # Red
-export COLOR_03="#74DFC4"           # Green
+export COLOR_03="#AFD686"           # Green
 export COLOR_04="#FEAE87"           # Yellow
 export COLOR_05="#40B4C4"           # Blue
 export COLOR_06="#B381C5"           # Magenta
-export COLOR_07="#06989A"           # Cyan
-export COLOR_08="#D1D1D1"           # Light gray
+export COLOR_07="#215969"           # Cyan
+export COLOR_08="#91889b"           # Light gray
 
-export COLOR_09="#91889B"           # Dark gray
+export COLOR_09="#716485"           # Dark gray
 export COLOR_10="#FC2377"           # Light Red
 export COLOR_11="#50FA7B"           # Light Green
 export COLOR_12="#FFE261"           # Light Yellow
-export COLOR_13="#40B4C4"           # Light Blue
+export COLOR_13="#74DFC4"           # Light Blue
 export COLOR_14="#6D75E0"           # Light Magenta
 export COLOR_15="#B4DCE7"           # Light Cyan
 export COLOR_16="#FFFFFF"           # White
 
 export BACKGROUND_COLOR="#1F1926"   # Background Color
-export FOREGROUND_COLOR="#C7E1E4"   # Foreground Color (text)
+export FOREGROUND_COLOR="#E0E0E0"   # Foreground Color (text)
 export CURSOR_COLOR="#C7C7C7"       # Cursor color
 export PROFILE_NAME="laserwave"
 # =============================================================== #
