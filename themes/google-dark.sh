b8b1d55287173b661f47bac508a3c303767164b5
Daniel Neal <dan.r.neal@gmail.com>
2021-04-20 08:45:49 -0700

    Update google-dark.sh

--- themes/google-dark.sh
+++ themes/google-dark.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env bash
 
 # ====================CONFIG THIS =============================== #
-export COLOR_01="#000000"           # Black
+export COLOR_01="#202124"           # Black
 export COLOR_02="#EA4335"           # Red
 export COLOR_03="#34A853"           # Green
 export COLOR_04="#FBBC05"           # Yellow
 export COLOR_05="#4285F4"           # Blue
 export COLOR_06="#A142F4"           # Cyan
 export COLOR_07="#24C1E0"           # Purple
-export COLOR_08="#EEEEEE"           # Light gray
+export COLOR_08="#E8EAED"           # Light gray
 
-export COLOR_09="#656565"           # Dark gray
+export COLOR_09="#5F6368"           # Dark gray
 export COLOR_10="#EA4335"           # Bright Red
 export COLOR_11="#34A853"           # Bright Green
 export COLOR_12="#FBBC05"           # Bright Yellow
@@ -19,8 +19,8 @@ export COLOR_14="#A142F4"           # Bright Cyan
 export COLOR_15="#24C1E0"           # Bright Purple
 export COLOR_16="#FFFFFF"           # White
 
-export BACKGROUND_COLOR="#000000"   # Background Color
-export FOREGROUND_COLOR="#EEEEEE"   # Foreground Color (text)
+export BACKGROUND_COLOR="#202124"   # Background Color
+export FOREGROUND_COLOR="#E8EAED"   # Foreground Color (text)
 export CURSOR_COLOR="$FOREGROUND_COLOR" # Cursor color
 export PROFILE_NAME="Google Dark"
 # =============================================
