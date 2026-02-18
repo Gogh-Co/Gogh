3351ac3106671fcf218c02c81fbb48b69d6fcbbe
Mgldvd <mgldvd@pm.me>
2026-02-18 18:55:37 -0500

    chore: remove manifest link from head in app.vue

--- src/app.vue
+++ src/app.vue
@@ -20,7 +20,6 @@ useHead({
         { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicons/favicon-32x32.png' },
         { rel: 'icon', type: 'image/png', sizes: '96x96', href: '/favicons/favicon-96x96.png' },
         { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicons/favicon-16x16.png' },
-        { rel: 'manifest', href: '/favicons/manifest.json' },
     ],
     meta: [
         { name: 'msapplication-TileColor', content: '#ffffff' },
