cbc0046b28f372e3629a665079855c942a5cfb0c
Mgldvd <mgldvd@pm.me>
2026-02-18 18:19:57 -0500

    feat(Button): add Button component with template, script, and styles

---
+++ src/components/Buttons/Button.vue
@@ -0,0 +1,30 @@
+<template>
+    <a
+        class="btn gogh-btn"
+        :class="[{ active }, extraClass]"
+        :href="href || undefined"
+    >
+        <slot />
+    </a>
+</template>
+
+<script setup>
+defineProps({
+    active: {
+        type: Boolean,
+        default: false,
+    },
+    extraClass: {
+        type: String,
+        default: '',
+    },
+    href: {
+        type: String,
+        default: '',
+    },
+});
+</script>
+
+<style lang="scss" scoped>
+@use './Button.scss';
+</style>
