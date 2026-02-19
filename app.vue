5b722ac6d0eab164dafa4775055f62fb8468dc8b
mgldvd <mgldvd@pm.me>
2026-02-19 10:19:48 -0500

    refactor: remove manifest link from head in app.vue

--- app.vue
+++ app.vue
@@ -28,7 +28,6 @@ useHead({
         { rel: 'icon', type: 'image/png', sizes: '32x32', href: withBase('favicons/favicon-32x32.png') },
         { rel: 'icon', type: 'image/png', sizes: '96x96', href: withBase('favicons/favicon-96x96.png') },
         { rel: 'icon', type: 'image/png', sizes: '16x16', href: withBase('favicons/favicon-16x16.png') },
-        { rel: 'manifest', href: withBase('favicons/manifest.json') },
     ],
     meta: [
         { name: 'msapplication-TileColor', content: '#ffffff' },
