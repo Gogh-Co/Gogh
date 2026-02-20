5d542d9697d62a2785edd189f8bf8179815f6d2a
mgldvd <mgldvd@pm.me>
2026-02-20 07:10:50 -0500

    feat: add SVG icons to buttons for enhanced visual appeal

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -117,6 +117,7 @@
                             target="_blank"
                             extra-class="btn-bk">
                             Generator
+                            <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><!-- Icon from Remix Icon by Remix Design - https://github.com/Remix-Design/RemixIcon/blob/master/License --><path fill="currentColor" d="M10 6v2H5v11h11v-5h2v6a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V7a1 1 0 0 1 1-1zm11-3v8h-2V6.413l-7.793 7.794l-1.414-1.414L17.585 5H13V3z"/></svg>
                         </Button>
 
                         <Button
@@ -124,6 +125,7 @@
                             target="_blank"
                             extra-class="btn-bk">
                             WCSG
+                            <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><!-- Icon from Remix Icon by Remix Design - https://github.com/Remix-Design/RemixIcon/blob/master/License --><path fill="currentColor" d="M10 6v2H5v11h11v-5h2v6a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V7a1 1 0 0 1 1-1zm11-3v8h-2V6.413l-7.793 7.794l-1.414-1.414L17.585 5H13V3z"/></svg>
                         </Button>
 
                         <div class="theme-search" role="search">
